8f7f39f344989ca84fe5bd991b5af82bc97c06fb
unknown <enzzomatrix@gmail.com>
2025-02-21 13:03:26 +1100

    upd

---
+++ 01_guides/01/springdatajpa/src/main/java/ru/vasilev/springdatajpa/SpringdatajpaApplication.java
@@ -0,0 +1,13 @@
+package ru.vasilev.springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpaApplication.class, args);
+	}
+
+}
